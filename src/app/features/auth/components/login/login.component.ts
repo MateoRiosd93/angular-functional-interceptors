4f425380ee69d174176158764b084113f9e76bb1
marios <marios@bancolombia.com.co>
2025-06-06 13:51:08 -0500

    feat(login): Se crea estrucutra del formulario, con ReactiveFormsModule y se ajusta el html para enlazarlo con el formulario

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
+import { AuthService } from '../../services/auth.service';
+import { LoginRequest } from '../../models/login-request.model';
+
+interface LoginForm {
+    username: FormControl<string>
+    password: FormControl<string>
+}
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    selector: 'app-login',
+    imports: [ReactiveFormsModule],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    private readonly formBuilder = inject(FormBuilder)
+    private readonly authService = inject(AuthService)
 
+    form = this.formBuilder.group<LoginForm>({
+        username: this.formBuilder.control('', { nonNullable: true, validators: [Validators.required] }),
+        password: this.formBuilder.control('', { nonNullable: true, validators: [Validators.required] })
+    })
+
+    login() {
+        if (this.form.invalid) return
+
+        const { username = '', password = '' } = this.form.value
+
+        const request: LoginRequest = {
+            username,
+            password
+        }
+
+        this.authService.logIn(request).subscribe({
+            next: response => console.log(response)
+        })
+    }
 }

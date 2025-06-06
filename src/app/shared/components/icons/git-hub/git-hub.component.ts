b58e43c29ff24a8969a3722b1f84cb2c4fef8a57
marios <marios@bancolombia.com.co>
2025-06-05 20:07:26 -0500

    feat(icons): Se crea componente para utilizar svg del logo de GitHub

---
+++ src/app/shared/components/icons/git-hub/git-hub.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-git-hub',
+  imports: [],
+  templateUrl: './git-hub.component.html',
+  styleUrl: './git-hub.component.css'
+})
+export class GitHubComponent {
+
+}

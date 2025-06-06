7b35f2182b7a3eaaef6b7067193bb936974ad970
marios <marios@bancolombia.com.co>
2025-06-06 13:51:50 -0500

    feat(service): Se ajusta el servicio del login con el adapter y se tipan tanto el request como el response

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { CredentialsRequest } from '../models/credentials-request';
+import { LoginRequest } from '../models/login-request.model';
 import { URL_BASE } from '../../../tokens/url-base.token';
+import { map, Observable } from 'rxjs';
+import { LoginResponse } from '../models/login-response.model';
+import { adaptLoginResponse } from '../adapters/login.adapter';
+import { LoginApiResponse } from '../models/login-api-response.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  private readonly httpClient = inject(HttpClient)
-  private readonly URL_BASE = inject(URL_BASE)
+    private readonly httpClient = inject(HttpClient)
+    private readonly URL_BASE = inject(URL_BASE)
 
-  logIn(credentials: CredentialsRequest) {
-    return this.httpClient.post(`${this.URL_BASE}/auth/login`, credentials)
-  }
+    logIn(request: LoginRequest): Observable<LoginResponse> {
+        return this.httpClient.post<LoginApiResponse>(`${this.URL_BASE}/auth/login`, request)
+            .pipe(map(adaptLoginResponse))
+    }
 }

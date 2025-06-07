d27539599d928c7946a2e7fa9db0d6c5ee92d1f0
marios <marios@bancolombia.com.co>
2025-06-07 10:08:08 -0500

    feat(adapter): Se adapter para transformar la informacion que llega del api de productos a como lo desea el front

---
+++ src/app/features/products/adapters/product.adapeter.ts
@@ -0,0 +1,12 @@
+import { ProductsApiResponse } from "../models/products-api-response.model";
+import { ProductsResponse } from "../models/products-response.model";
+
+export const adaptProductsResponse = (response: ProductsApiResponse): ProductsResponse[] => {
+    return response.products.map(product => ({
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        category: product.category,
+        images: product.images
+    }))
+}

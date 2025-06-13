944ed2a9b5a007cd7168dff7e63df8a72d3e58f2
marios <marios@bancolombia.com.co>
2025-06-13 09:39:39 -0500

    feat(store): Se ajusta modelo del store para agregar el producto seleccionado, o al que se desea ver en detalle

--- src/app/features/products/models/products-state.model.ts
+++ src/app/features/products/models/products-state.model.ts
@@ -1,13 +1,16 @@
+import { ProductResponse } from './product-response.model';
 import { ProductsResponse } from './products-response.model';
 
 export interface ProductsState {
     products: ProductsResponse[]
+    product: ProductResponse
     loading: boolean
     error: boolean
 }
 
 export const initialProductsState: ProductsState = {
     products: [],
+    product: {} as ProductResponse,
     loading: false,
     error: false
 }

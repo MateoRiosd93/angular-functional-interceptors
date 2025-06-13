8ed0a17ab41ff121e31f5c9b197180e0943abf62
marios <marios@bancolombia.com.co>
2025-06-12 21:00:48 -0500

    feat(adapter): Se ajusta modo del retrun del adapter

--- src/app/features/auth/adapters/login.adapter.ts
+++ src/app/features/auth/adapters/login.adapter.ts
@@ -1,12 +1,10 @@
 import { LoginApiResponse } from '../models/login-api-response.model';
 import { LoginResponse } from '../models/login-response.model';
 
-export const adaptLoginResponse = (response: LoginApiResponse): LoginResponse => {
-    return {
-        accessToken: response.accessToken,
-        username: response.username,
-        email: response.email,
-        gender: response.gender,
-        image: response.image,
-    }
-}
+export const adaptLoginResponse = (response: LoginApiResponse): LoginResponse => ({
+    accessToken: response.accessToken,
+    username: response.username,
+    email: response.email,
+    gender: response.gender,
+    image: response.image,
+})

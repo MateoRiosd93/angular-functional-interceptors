d687a46ca6e97ffd6db0aafe9ba927ed2a9f9266
marios <marios@bancolombia.com.co>
2025-06-12 21:02:38 -0500

    feat(service): Se crea service para obtener el detalle de un producto

--- src/app/features/products/services/products.service.ts
+++ src/app/features/products/services/products.service.ts
@@ -2,9 +2,12 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { URL_BASE } from "../../../tokens/url-base.token";
 import { map, Observable } from "rxjs";
-import { adaptProductsResponse } from "../adapters/product.adapeter";
+import { adaptProductsResponse } from "../adapters/products.adapeter";
 import { ProductsResponse } from "../models/products-response.model";
 import { ProductsApiResponse } from "../models/products-api-response.model";
+import { ProductResponse } from "../models/product-response.model";
+import { ProductApiResponse } from "../models/product-api-response.model";
+import { adpatProductResponse } from "../adapters/product.adapter";
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
@@ -16,6 +19,9 @@ export class ProductsService {
             .pipe(map(adaptProductsResponse))
     }
 
-    // TODO: Realizar obtener detalle del producto
+    getProductDetail(id: number): Observable<ProductResponse>{
+        return this.httpClient.get<ProductApiResponse>(`${this.URL_BASE}/products/${id}`)
+            .pipe(map(adpatProductResponse))
+    }
     
 }

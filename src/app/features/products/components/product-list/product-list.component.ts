ce387f7a5cc8cb89be65011227277bd2bdb9ba7b
marios <marios@bancolombia.com.co>
2025-06-14 10:30:29 -0500

    feat(list): Se maqueta mejor el listado de los productos y se hace la navegacion al detalle de cada producto

--- src/app/features/products/components/product-list/product-list.component.ts
+++ src/app/features/products/components/product-list/product-list.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsStore } from '../../store/products.store';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-product-list',
-  imports: [],
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+    selector: 'app-product-list',
+    imports: [],
+    templateUrl: './product-list.component.html',
+    styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+    readonly productsStore = inject(ProductsStore)
+    private readonly router = inject(Router)
 
+    ngOnInit() {
+        this.productsStore.getProducts()
+    }
+
+    showProductDetail(id: number){
+        this.productsStore.getProductDetails(id)
+        this.router.navigateByUrl(`/products/${id}`)
+    }
 }
+

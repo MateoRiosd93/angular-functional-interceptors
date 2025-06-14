c3df5b995cc67d3b61739263ed87a1a7cc267648
marios <marios@bancolombia.com.co>
2025-06-14 10:29:33 -0500

    feat(store): Se crea productsStore para manejar el estado de los productos

--- src/app/features/products/store/products.store.ts
+++ src/app/features/products/store/products.store.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable, signal } from "@angular/core";
+import { computed, inject, Injectable, signal } from "@angular/core";
 import { ProductsService } from "../services/products.service";
 import { initialProductsState, ProductsState } from "../models/products-state.model";
 
@@ -7,5 +7,52 @@ export class ProductsStore {
     private readonly productsService = inject(ProductsService)
     private readonly state = signal<ProductsState>(initialProductsState)
 
-    // TODO: Realizar los computed signlas y y sus acciones.
+    // Computed signals (Solo son de lectura)
+    readonly products = computed(() => this.state().products)
+    readonly product = computed(() => this.state().product)
+    readonly loading = computed(() => this.state().loading)
+    readonly error = computed(() => this.state().error)
+
+    // Acciones para controlar el state
+    getProducts() {
+        this.state.update(state => ({ ...state, loading: true }))
+
+        this.productsService.getProducts().subscribe({
+            next: products => this.state.update(state => ({
+                ...state,
+                products,
+                loading: false
+            })),
+            error: error => {
+                this.state.update(state => ({
+                    ...state,
+                    loading: false,
+                    error: true
+                }))
+
+                console.error(error)
+            }
+        })
+    }
+
+    getProductDetails(id: number) {
+        this.state.update(state => ({ ...state, loading: true }))
+
+        this.productsService.getProductDetail(id).subscribe({
+            next: product => this.state.update(state => ({
+                ...state,
+                product,
+                loading: false
+            })),
+            error: error => {
+                this.state.update(state => ({
+                    ...state,
+                    loading: false,
+                    error: true
+                }))
+
+                console.error(error)
+            }
+        })
+    }
 }

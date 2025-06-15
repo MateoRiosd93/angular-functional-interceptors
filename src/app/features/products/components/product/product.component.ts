d31de863cf0a930b401c7ac15f911b86a56e377a
marios <marios@bancolombia.com.co>
2025-06-15 08:49:38 -0500

    feat(product): Se implementa la accion del detalle del producto, ademas de agregar el ActivatedRoute para obtener el parametro de la ruta.

--- src/app/features/products/components/product/product.component.ts
+++ src/app/features/products/components/product/product.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsStore } from '../../store/products.store';
+import { ActivatedRoute } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-product',
-  imports: [],
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+    selector: 'app-product',
+    imports: [CurrencyPipe],
+    templateUrl: './product.component.html',
+    styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+    readonly productsStore = inject(ProductsStore)
+    private readonly route = inject(ActivatedRoute)
 
+    ngOnInit(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'))
+        this.productsStore.getProductDetails(id).subscribe()
+        setTimeout(() => {
+            console.log(this.productsStore.product());
+        }, 1000);
+    }
 }

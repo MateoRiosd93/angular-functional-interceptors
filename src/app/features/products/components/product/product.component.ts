2e778db44483a8acf0e53d217a39125750308d1a
marios <marios@bancolombia.com.co>
2025-06-16 08:56:08 -0500

    feat(product): Se estructura un poco el detalle del producto

--- src/app/features/products/components/product/product.component.ts
+++ src/app/features/products/components/product/product.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsStore } from '../../store/products.store';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
+import { BackComponent } from '../../../../shared/components/icons/back/back.component';
 
 @Component({
     selector: 'app-product',
-    imports: [CurrencyPipe],
+    imports: [CurrencyPipe, RouterLink, BackComponent],
     templateUrl: './product.component.html',
     styleUrl: './product.component.css'
 })

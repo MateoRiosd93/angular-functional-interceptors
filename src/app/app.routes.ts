c621b5e818b1967af31a3bb76ba4178558c5794a
marios <marios@bancolombia.com.co>
2025-06-17 08:44:38 -0500

    feat(guard): Se hace uso del canActivate para las rutas publicas y privadas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,22 @@
 import { Routes } from '@angular/router';
+import { privateGuard, publicGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
+        canActivate: [publicGuard],
         loadChildren: () => import('./features/home/home.routes')
             .then(routesModule => routesModule.default)
     },
     {
         path: 'auth',
+        canActivate: [publicGuard],
         loadChildren: () => import('./features/auth/auth.routes')
             .then(routesModule => routesModule.default)
     },
     {
         path: 'products',
+        canActivate: [privateGuard],
         loadChildren: () => import('./features/products/products.routes')
             .then(routerModule => routerModule.default)
     },
--- src/app/features/products/products.routes.ts
+++ src/app/features/products/products.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from "@angular/router";
+import { privateGuard } from "../../core/guards/auth.guard";
 
 export default [
     {
         path: 'list',
+        canActivate: [privateGuard],
         loadComponent: () => import('./components/product-list/product-list.component')
             .then(componentModule => componentModule.ProductListComponent)
     },
     {
         path: ':id',
+        canActivate: [privateGuard],
         loadComponent: () => import('./components/product/product.component')
             .then(componentModule => componentModule.ProductComponent)
     }

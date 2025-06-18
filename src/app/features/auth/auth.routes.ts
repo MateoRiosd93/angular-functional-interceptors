5352105c2d24cfb1041a7dfcfdbaa57cfed7bd7e
marios <marios@bancolombia.com.co>
2025-06-18 18:58:48 -0500

    feat(guard): Se agregan guards en rutas para ser mas especificos en cuanto a las rutas publicas y privadas

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
+import { publicGuard } from "../../core/guards/auth.guard";
 
 export default [
     {
         path: 'log-in',
+        canActivate: [publicGuard],
         loadComponent: () => import('./components/login/login.component')
             .then(componentModule => componentModule.LoginComponent)
     },
--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
+import { publicGuard } from "../../core/guards/auth.guard";
 
 export default [
     {
         path: '',
+        canActivate: [publicGuard],
         loadComponent: () => import('./components/home/home.component')
             .then(componentModule => componentModule.HomeComponent)
     }

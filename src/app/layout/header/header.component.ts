5f7878f5eb2ada1c2f8b08bc6ab6ef2348795bf4
marios <marios@bancolombia.com.co>
2025-06-18 19:02:50 -0500

    feat(header): Se crea metodo para hacer el logout de la app

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GitHubComponent } from '../../shared/components/icons/git-hub/git-hub.component';
+import { UserStore } from '../../features/auth/store/user.store';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  imports: [GitHubComponent],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    imports: [GitHubComponent, RouterLink],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+    readonly userStore = inject(UserStore)
 
+    logOut(){
+        localStorage.clear()
+        this.userStore.clearUserStore()
+    }
 }
